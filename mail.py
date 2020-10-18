b39e504a157df792040925def7f4d2d7fa430985
Shamanche <72706516+Shamanche@users.noreply.github.com>
2020-10-18 23:04:32 +0300

    add app.config for mail

--- mail.py
+++ mail.py
@@ -3,3 +3,10 @@ from flask_mail import Mail, Message
 
 app = Flask(__name__)
 mail = Mail(app)
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'test@gmail.com'  # введите свой адрес электронной почты здесь
+app.config['MAIL_DEFAULT_SENDER'] = 'test@gmail.com'  # и здесь
+app.config['MAIL_PASSWORD'] = 'password'  # введите пароль
