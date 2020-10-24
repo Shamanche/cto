c375b3c2fff2f34e27ae6f64a3562fad1e7406aa
Shamanche <72706516+Shamanche@users.noreply.github.com>
2020-10-24 15:32:08 +0300

    add sending mail with mail.py

--- mail.py
+++ mail.py
@@ -2,11 +2,17 @@ from flask import Flask
 from flask_mail import Mail, Message
 
 app = Flask(__name__)
-mail = Mail(app)
 
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'test@gmail.com'  # введите свой адрес электронной почты здесь
-app.config['MAIL_DEFAULT_SENDER'] = 'test@gmail.com'  # и здесь
-app.config['MAIL_PASSWORD'] = 'password'  # введите пароль
+app.config['MAIL_SERVER'] = 'smtp.yandex.ru'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'informersmart@yandex.ru'  # введите свой адрес электронной почты здесь
+app.config['MAIL_DEFAULT_SENDER'] = 'informersmart@yandex.ru'  # и здесь
+app.config['MAIL_PASSWORD'] = 'Smart620514'  # введите пароль
+
+with app.app_context():
+    mail = Mail(app)
+    msg = Message("Subject", recipients=['tsvet005@yandex.ru'])
+    msg.body = ' test message body'
+    mail.send(msg)
